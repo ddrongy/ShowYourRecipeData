1c1ff2d0edb60222dfce6edbe66fb9b4b99de674
ddrongy <ddrongy@gmail.com>
2022-09-23 14:30:56 +0900

    [feat] make crawling .py file

--- Crawling/crawling_user.py
+++ Crawling/crawling_user.py
@@ -13,6 +13,9 @@ def crawling(rangelen, startnum, savename):
 
         elements = driver.find_elements_by_xpath('//*[@id="contents_area_full"]/ul/ul/li')
         elements_len = len(elements)
+        
+        if elements_len == 0:
+            break
 
         for i in range(elements_len):
             url_link = driver.find_elements_by_xpath('//*[@id="contents_area_full"]/ul/ul/li['+(str(i+1))+']/div[1]/a')
@@ -21,16 +24,16 @@ def crawling(rangelen, startnum, savename):
             title = title[0].text
             name = driver.find_elements_by_xpath('//*[@id="contents_area_full"]/ul/ul/li['+(str(i+1))+']/div[2]/div[2]')
             name= name[0].text
-            df.loc[len(df)] = [url_link, title, name ]
+            df.loc[len(df)] = [url_link, title, name]
         df.to_csv(savename)
         
         
 def main():
-    # crawling(1000,1,'10000recipe1.csv')
+    crawling(1000,1,'10000recipe1.csv')
     crawling(1000,1001,'10000recipe2.csv')
     crawling(1000,2001,'10000recipe3.csv')
     crawling(1000,3001,'10000recipe4.csv')
-    crawling(644,4001,'10000recipe5.csv')
+    crawling(650,4001,'10000recipe5.csv')
 
 if __name__ == "__main__":
     main()
